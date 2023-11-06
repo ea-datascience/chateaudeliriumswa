b31cf6b116f8d20c7f091e6d8e4b1fc2fc402f37
Enrique Acuay <e.acuay@gmail.com>
2023-11-06 23:57:32 +0100

    adding two modules to the new iteration of the application

--- chateaudelirium/src/app/app-routing.module.ts
+++ chateaudelirium/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home', 
+    loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path:'trainer', 
+    loadChildren: () => import('./trainer/trainer.module').then(m => m.TrainerModule)
+  },
+
+  {
+    path:'**', 
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
