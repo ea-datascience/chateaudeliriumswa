0a9fa09f284f5c15e4b9ca2cf1dac87eeff412c3
Enrique Acuay <e.acuay@gmail.com>
2023-11-10 17:49:27 +0100

    adding the signup buttons using the standard firebase style

--- chateaudelirium/src/app/register-trainer/auth-ui/auth-ui.component.ts
+++ chateaudelirium/src/app/register-trainer/auth-ui/auth-ui.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth-ui',
   templateUrl: './auth-ui.component.html',
-  styleUrls: ['./auth-ui.component.css']
+  styleUrls: ['./auth-ui.component.css','./firebase-ui-auth.css']
 })
 export class AuthUIComponent {
+  googleLogin(){
+  }
 
+  appleLogin(){
+  }
+
+  emailLogin(email:string, password:string){
+  }
 }
