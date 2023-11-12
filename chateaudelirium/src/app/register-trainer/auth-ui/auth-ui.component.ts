dbf29ee488b6a188627862218c847fdd6719551b
Enrique Acuay <e.acuay@gmail.com>
2023-11-12 16:32:24 +0100

    adding github and microsoft login, removing all the rest until they are active

--- chateaudelirium/src/app/register-trainer/auth-ui/auth-ui.component.ts
+++ chateaudelirium/src/app/register-trainer/auth-ui/auth-ui.component.ts
@@ -11,26 +11,22 @@ export class AuthUIComponent {
   constructor(private http:HttpClient) { }
 
   githubLogin(){
+    window.open('/.auth/login/github', '_self');
   }
-
+  
   googleLogin(){
   }
-
+  
   appleLogin(){
   }
-
+  
   emailLogin(email:string, password:string){
   }
-
+  
   // https://black-tree-0badee803.4.azurestaticapps.net/home
-
+  
   microsoftLogin(){
-    window.open('/.auth/login/github', '_self');
-    /**
-    this.http.get('https://black-tree-0badee803.4.azurestaticapps.net/.auth/login/aad').subscribe((res:any) => {
-      window.location.href = res.redirectUrl;
-    } );  
-    */
+    window.open('/.auth/login/aad', '_self');
   }
   
 }
