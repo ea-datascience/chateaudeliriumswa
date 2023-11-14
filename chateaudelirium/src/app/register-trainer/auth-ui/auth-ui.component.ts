a9088236c9480af4c3f0b86e9f6a6e89e5016a58
Enrique Acuay <e.acuay@gmail.com>
2023-11-15 00:31:27 +0100

    test retrieval of the user information after the github login

--- chateaudelirium/src/app/register-trainer/auth-ui/auth-ui.component.ts
+++ chateaudelirium/src/app/register-trainer/auth-ui/auth-ui.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UserInfo } from '../models/user-info';
 
 @Component({
   selector: 'app-auth-ui',
@@ -12,8 +13,10 @@ export class AuthUIComponent implements OnInit{
 
   constructor(private http:HttpClient) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
     console.log(this.redirect);
+    this.userInfo = await this.getUserInfo();
+    console.log(this.userInfo);
   }
 
   githubLogin(){
@@ -33,5 +36,22 @@ export class AuthUIComponent implements OnInit{
   microsoftLogin(){
     window.open('/.auth/login/aad', '_self');
   }
+
+  //------------------------------------------------------------
+
+  userInfo!: UserInfo | undefined;
+
+
+  async getUserInfo(): Promise<UserInfo | undefined> {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (err) {
+      console.error('Failed to get user information', err);
+      return undefined;
+    }
+  }  
   
 }
